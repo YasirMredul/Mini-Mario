62d2b4bf15938b3d36a56acf139011458a91cc66
ayesha163 <18101001@uap-bd.edu>
2021-03-07 18:58:56 +0600

    key add

--- gameloop.py
+++ gameloop.py
@@ -1,12 +1,12 @@
 def game_loop():
-    while True:
-        global dragon
-        dragon = Dragon()
-        flames = Flames()
-        mario = Mario()
-        add_new_flame_counter = 0
+    while True:unter
         global SCORE
         SCORE = 0
+        global dragon
+        dragon = Dragon() #call class
+        flames = Flames()
+        mario = Mario()
+        add_new_flame_counter = 0 #intial value of counter
         global HIGH_SCORE
         flames_list = []
 
@@ -14,14 +14,33 @@ def game_loop():
             canvas.fill(BLACK)
             check_level(SCORE)
             dragon.update()
-            add_new_flame_counter += 1
+            add_new_flame_counter += 1 #update flame counter
 
-            if add_new_flame_counter == ADD_NEW_FLAME_RATE:
+            if add_new_flame_counter == ADD_NEW_FLAME_RATE: #check previous
                 add_new_flame_counter = 0
                 new_flame = Flames()
-                flames_list.append(new_flame)
+                flames_list.append(new_flame) #update flamelist
             for f in flames_list:
-                if f.flames_img_rect.left <= 0:
-                    flames_list.remove(f)
-                    SCORE += 1
+                if f.flames_img_rect.left <= 0: #check it hits mario or pass
+                    flames_list.remove(f)     #remove this flame from flame array
+                    SCORE += 1 #add score
                 f.update()
+
+                for event in pygame.event.get(): #builtin function to check leaving game
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == pygame.KEYDOWN: #mario keydown
+                        if event.key == pygame.K_UP:
+                            mario.up = True
+                            mario.down = False
+                        elif event.key == pygame.K_DOWN:
+                            mario.down = True
+                            mario.up = False
+                    if event.type == pygame.KEYUP: #mario up
+                        if event.key == pygame.K_UP:
+                            mario.up = False
+                            mario.down = True
+                        elif event.key == pygame.K_DOWN:
+                            mario.down = True
+                            mario.up = False
